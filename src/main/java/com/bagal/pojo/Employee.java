58479e3f85a14cbb4aa78cd50e5a6687a246d983
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-01-31 23:58:40 +0530

    updated lombok to string annotation

--- src/main/java/com/bagal/pojo/Employee.java
+++ src/main/java/com/bagal/pojo/Employee.java
@@ -2,26 +2,20 @@ package com.bagal.pojo;
 
 import com.creditdatamw.zerocell.annotation.Column;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class Employee {
     @Column(name = "empId",index = 0)
     private String empId;
+
     @Column(name = "firstName",index = 1)
     private String firstName;
 
     @Column(name = "lastName",index = 2)
     private String lastName;
+
     @Column(name = "email",index = 3)
     private String email;
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "empId='" + empId + '\'' +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
 }
